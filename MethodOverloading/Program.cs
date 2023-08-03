39fdc0196e3a990f7fff49db31d47ab816745b43
RafaelAdrianMejia <tucsonrafa@outlook.com>
2023-08-03 12:31:19 -0700

    exercise completed

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,49 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int x = 30;
+            int y = 50;
+
+            var intAdd = Add(x, y);
+            Console.WriteLine(intAdd);
+
+            var decAdd = Add(100.50m, 200m);
+            Console.WriteLine(decAdd);
+
+            var boolAdd = Add(2, 2 , true);
+            Console.WriteLine(boolAdd);
+        }
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+
+        public static string Add(int num1, int num2, bool isChecked)
+        {
+            int sum = num1 + num2;
+
+            if(isChecked && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+            else if (isChecked && sum == 1)
+            {
+                return $"{sum} dollar";   
+            }
+            else if(isChecked && sum < 1)
+            {
+                return "You are broke";
+            }
+            else
+            {
+                return sum.ToString();
+            }
         }
     }
 }
